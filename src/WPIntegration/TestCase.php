c93d217d34adea16f06fd0b3a391feb132090a90
jrfnl <jrfnl@users.noreply.github.com>
2020-12-09 09:46:47 +0100

    Add basic TestCase for use with the WP native test setup (integration tests)
    This TestCase:
    * Extends the WP native base test case `WP_UnitTestCase`.
    * Makes all relevant PHPUnit polyfills available to test classes implementing this test case.

---
+++ src/WPIntegration/TestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Yoast\WPTestUtils\WPIntegration;
+
+use WP_UnitTestCase;
+use Yoast\PHPUnitPolyfills\Helpers\AssertAttributeHelper;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertEqualsSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertFileEqualsSpecializations;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertionRenames;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertIsType;
+use Yoast\PHPUnitPolyfills\Polyfills\AssertStringContains;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectExceptionMessageMatches;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectExceptionObject;
+use Yoast\PHPUnitPolyfills\Polyfills\ExpectPHPException;
+
+/**
+ * Basic test case for use with a WP Integration test test suite.
+ *
+ * This test case extends the WordPress native base test case and makes
+ * all relevant polyfills available to allow for using PHPUnit 9.x
+ * assertion and expectation syntax.
+ *
+ * @since 0.2.0
+ */
+abstract class TestCase extends WP_UnitTestCase {
+
+	use AssertAttributeHelper;
+	use AssertEqualsSpecializations;
+	use AssertFileEqualsSpecializations;
+	use AssertionRenames;
+	use AssertIsType;
+	use AssertStringContains;
+	use ExpectExceptionMessageMatches;
+	use ExpectExceptionObject;
+	use ExpectPHPException;
+
+}
