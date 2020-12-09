bf70795103c363ea03355c15dd4efd703c64e77e
jrfnl <jrfnl@users.noreply.github.com>
2020-12-09 10:05:43 +0100

    Add bootstrap functions for integration tests
    The functionality within these files presumes three things:
    1. This package is installed as a dependency of a plugin via Composer.
    2. WordPress itself is available..
    3. Either the `WP_TESTS_DIR` environment variable or the `WP_DEVELOP_DIR` environment variable will be set.
        If neither is available, the plugin is presumed to be installed in a `src/wp-content/plugins/plugin-name` directory, with this package in the `src/wp-content/plugins/plugin-name/vendor/yoast/wp-test-utils` directory.
    
    This is in line with a typical integration test setup in the context of WordPress and in line with the integration tests as used within the Yoast organisation.
    
    When the above assumptions are satisfied, these files offer:
    * Utility functions for use in the test `bootstrap` file for a plugin.
    * A custom autoloader which will selectively autoload the WP copies of the PHPUnit 9 native MockBuilder files for PHPUnit < 9 when run on PHP 8 to get round the use of the new reserved keyword `match` as was used in older versions of these files.
    
    Code sample for how this functionality should typically be used:
    ```php
    if ( getenv( 'WP_PLUGIN_DIR' ) !== false ) {
        define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
    }
    
    $GLOBALS['wp_tests_options'] = [
        'active_plugins' => [ 'plugin-name/main-file.php' ],
    ];
    
    require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
    
    /*
     * Load WordPress, which will load the Composer autoload file, and load the MockObject autoloader after that.
     */
    WPIntegration\bootstrap_it();
    
    if ( ! defined( 'WP_PLUGIN_DIR' ) || file_exists( WP_PLUGIN_DIR . '/plugin-name/main-file.php' ) === false ) {
        echo PHP_EOL, 'ERROR: Please check whether the WP_PLUGIN_DIR environment variable is set and set to the correct value. The unit test suite won\'t be able to run without it.', PHP_EOL;
        exit( 1 );
    }
    ```
    
    Includes minor tweaks to the PHPCS configuration.
    
    Includes ignoring the functionality within these files for code coverage as the above assumptions will not be satisfied when this package is tested stand-alone.

---
+++ src/WPIntegration/Autoload.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Yoast\WPTestUtils\WPIntegration;
+
+use PHPUnit\Runner\Version as PHPUnit_Version;
+
+/**
+ * Custom autoloader.
+ *
+ * Autoloader file for the PHPUnit 9 MockObject classes.
+ *
+ * Hack around PHPUnit < 9.3 mocking not being compatible with PHP 8.
+ *
+ * This allows for cross-version compatibility with various PHP, PHPUnit and WP versions.
+ *
+ * Use the `Yoast\WPTestUtils\WPIntegration\register_mockobject_autoloader()` function
+ * as defined in the `src/WPIntegration/bootstrap-functions.php` file to register the autoloader.
+ *
+ * @since 0.2.0
+ */
+final class Autoload {
+
+	/**
+	 * A list of the classes this autoloader handles.
+	 *
+	 * @var string[] => true
+	 */
+	private static $supported_classes = [
+		'PHPUnit\Framework\MockObject\Builder\NamespaceMatch'  => true,
+		'PHPUnit\Framework\MockObject\Builder\ParametersMatch' => true,
+		'PHPUnit\Framework\MockObject\InvocationMocker'        => true,
+		'PHPUnit\Framework\MockObject\MockMethod'              => true,
+	];
+
+	/**
+	 * Loads a class.
+	 *
+	 * @param string $class The name of the class to load.
+	 *
+	 * @return bool
+	 */
+	public static function load( $class ) {
+
+		if ( \PHP_VERSION_ID < 80000 ) {
+			// This autoloader is only needed when the tests are being run on PHP >= 8.0.
+			// Let the standard Composer autoloader handle things otherwise.
+			return false;
+		}
+
+		if ( \class_exists( PHPUnit_Version::class ) === false
+			|| \version_compare( PHPUnit_Version::id(), '8.0.0', '>=' )
+		) {
+			// This autoloader is only needed when the tests are being run on PHPUnit < 8
+			// and won't work with PHPUnit 5 anyway.
+			return false;
+		}
+
+		if ( isset( self::$supported_classes[ $class ] ) === false ) {
+			// Bow out, not a class this autoloader handles.
+			return false;
+		}
+
+		$wp_test_dir = namespace\get_path_to_wp_test_dir();
+		if ( $wp_test_dir === false ) {
+			// We don't know where WP is installed...
+			return false;
+		}
+
+		// Try getting the overloaded file as included in WP 5.6/master.
+		$relative_filename = \strtr( \substr( $class, 18 ), '\\', \DIRECTORY_SEPARATOR ) . '.php';
+		$file              = \realpath( $wp_test_dir . 'includes/phpunit7/' . $relative_filename );
+
+		if ( $file === false || @\file_exists( $file ) === false ) {
+			return false;
+		}
+
+		require_once $file;
+		return true;
+	}
+}
---
+++ src/WPIntegration/bootstrap-functions.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * PHPUnit bootstrap file for tests based on the WP native testing framework.
+ *
+ * @package Yoast\WPTestUtils
+ *
+ * @since 0.2.0
+ */
+
+namespace Yoast\WPTestUtils\WPIntegration;
+
+/**
+ * Retrieves the path to the WordPress `tests/phpunit/` directory.
+ *
+ * The path will be determined based on the following, in this order:
+ * - The `WP_TESTS_DIR` environment variable, if set.
+ *   This environment variable can be set in the OS or via a custom `phpunit.xml` file
+ *   and should point to the `tests/phpunit` directory of a WordPress clone.
+ * - The `WP_DEVELOP_DIR` environment variable, if set.
+ *   This environment variable can be set in the OS or via a custom `phpunit.xml` file
+ *   and should point to the root directory of a WordPress clone.
+ * - The plugin potentially being installed in a WordPress install.
+ *   In that case, the plugin is expected to be in the `src/wp-content/plugin/plugin-name` directory.
+ *
+ * Note: The path will be checked to make sure it is a valid path and actually points to
+ * a WP core install containing the `tests/phpunit/` directory.
+ *
+ * @return string|false Path to the WP `tests/phpunit/` directory (including trailing slash)
+ *                      or FALSE if the path couldn't be determined or if the path *could*
+ *                      be determined, but doesn't exist.
+ */
+function get_path_to_wp_test_dir() {
+	/**
+	 * Normalizes all slashes in a file path to forward slashes.
+	 *
+	 * @param string $path File path.
+	 *
+	 * @return string The file path with normalized slashes.
+	 */
+	$normalize_path = static function( $path ) {
+		return \str_replace( '\\', '/', $path );
+	};
+
+	if ( \getenv( 'WP_TESTS_DIR' ) !== false ) {
+		$tests_dir = \getenv( 'WP_TESTS_DIR' );
+		$tests_dir = \realpath( $tests_dir );
+		if ( $tests_dir !== false ) {
+			$tests_dir = $normalize_path( $tests_dir ) . '/';
+			if ( \substr( $tests_dir, -15 ) === '/tests/phpunit/'
+				&& \is_dir( $tests_dir ) === true
+				&& @\file_exists( $tests_dir . 'includes/bootstrap.php' )
+			) {
+				return $tests_dir;
+			}
+		}
+	}
+
+	if ( \getenv( 'WP_DEVELOP_DIR' ) !== false ) {
+		$dev_dir = \getenv( 'WP_DEVELOP_DIR' );
+		$dev_dir = \realpath( $dev_dir );
+		if ( $dev_dir !== false ) {
+			$dev_dir = $normalize_path( $dev_dir ) . '/';
+			if ( \is_dir( $dev_dir ) === true
+				&& @\file_exists( $dev_dir . 'tests/phpunit/includes/bootstrap.php' )
+			) {
+				return $dev_dir . 'tests/phpunit/';
+			}
+		}
+	}
+
+	/*
+	 * If neither of the constants was set, check whether the plugin is installed
+	 * in `src/wp-content/plugins`. In that case, this file would be in
+	 * `src/wp-content/plugins/plugin-name/vendor/yoast/wp-test-utils/src/WPIntegration`.
+	 */
+	if ( @\file_exists( __DIR__ . '/../../../../../../../../../tests/phpunit/includes/bootstrap.php' ) ) {
+		$tests_dir = __DIR__ . '/../../../../../../../../../tests/phpunit/';
+		$tests_dir = \realpath( $tests_dir );
+		if ( $tests_dir !== false ) {
+			return $normalize_path( $tests_dir ) . '/';
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Loads the WP native integration test bootstrap and register a custom autoloader.
+ *
+ * Use this function to make sure the autoloaders are registered in the correct order.
+ *
+ * @return void
+ */
+function bootstrap_it() {
+	// Make sure the Composer autoload file has been generated.
+	namespace\check_composer_autoload_exists();
+
+	// Load WordPress.
+	namespace\load_wp_test_bootstrap();
+
+	/*
+	 * Register the custom autoloader to overload the PHPUnit MockObject classes when running on PHP 8.
+	 *
+	 * This function has to be called _last_, after the WP test bootstrap to make sure it registers
+	 * itself in FRONT of the Composer autoload (which also prepends itself to the autoload queue).
+	 */
+	namespace\register_mockobject_autoloader();
+}
+
+/**
+ * Registers a custom autoload file to use the WP overload MockBuilder classes when
+ * on PHP 8 combined with an incompatible PHPUnit version.
+ *
+ * @return void
+ */
+function register_mockobject_autoloader() {
+	if ( \class_exists( Autoload::class ) === false ) {
+		namespace\load_composer_autoload();
+	}
+
+	/*
+	 * Register the autoloader and prepend it before any existing autoloaders
+	 * to ensure that any PHPUnit overloaded files get loaded via this Autoloader,
+	 * not via the Composer one.
+	 */
+	\spl_autoload_register( __NAMESPACE__ . '\Autoload::load', true, true );
+}
+
+/**
+ * Loads the WordPress native test bootstrap file to set up the environment
+ * for integration tests.
+ *
+ * @return void
+ */
+function load_wp_test_bootstrap() {
+	$wp_test_path = namespace\get_path_to_wp_test_dir();
+
+	if ( $wp_test_path !== false ) {
+		// We can safely load the bootstrap file as the `Autoload::determine_wp_test_dir()` method
+		// already verifies it exists.
+		require_once $wp_test_path . 'includes/bootstrap.php';
+		return;
+	}
+
+	echo \PHP_EOL, 'ERROR: The WordPress native unit test bootstrap file could not be found. Please set either the WP_TESTS_DIR or the WP_DEVELOP_DIR environment variable, either in your OS or in a custom phpunit.xml file.', \PHP_EOL;
+	exit( 1 );
+}
+
+/**
+ * Load the Composer autoload file of a plugin which uses this library as a dependency.
+ *
+ * @return void
+ */
+function load_composer_autoload() {
+	namespace\check_composer_autoload_exists();
+	require_once __DIR__ . '/../../../../autoload.php';
+}
+
+/**
+ * Verifies whether the Composer autoload file exists for a plugin which uses this libary
+ * as a dependency.
+ *
+ * @return void
+ */
+function check_composer_autoload_exists() {
+	if ( @\file_exists( __DIR__ . '/../../../../autoload.php' ) === false ) {
+		echo \PHP_EOL, 'ERROR: Run `composer install` to generate the autoload files before running the unit tests.', \PHP_EOL;
+		exit( 1 );
+	}
+}
