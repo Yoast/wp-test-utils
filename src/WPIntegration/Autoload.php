18e7e979681164a529a7ca76387f1ed708a78a97
jrfnl <jrfnl@users.noreply.github.com>
2021-09-24 16:46:14 +0200

    Integration tests TestCase: double the class for full compat with all supported WP versions
    With the introduction of the PHPUnit Polyfills in WP 5.9 and the backport of the availability of the PHPUnit Polyfills + the snake case test wrappers to WP 5.2 - 5.8, integration tests for plugins/themes which test against WP 5.2 - 5.9 (and higher) can start using modern PHPUnit assertions and expectations, as well as switch over to using the `snake_case` fixture method names.
    
    There is a caveat to this however:
    * Plugins/themes which test against older WP versions (< 5.2) don't have access to either the polyfills or the snake_case fixture method wrappers.
    * Same goes for tests being run against the current WP `latest`, which is the last 5.8.x release (5.8.1), which doesn't yet include the changes.
    * Same goes for tests being run against all WP 5.2 - 5.8 minors which were released before today.
    
    This commit:
    * Introduces the snake_case fixture method wrappers to the WP integration test `TestCase`.
    * Introduces a second `TestCase` for the WP integration tests.
        The original `TestCase` is suitable for use for older WP versions which don't include the test changes from WP 5.9.
        The new `TestCase` is suitable for use with WP 5.9 and WP versions which *do* include the test changes from WP 5.9.
        The new `TestCase` does not include the Polyfills as those will be inherited from WordPress.
    * Both these test cases have exactly the same name `Yoast\WPTestUtils\WPIntegration\TestCase` so concrete test classes can extend this class safely.
        Because of this, this class will no longer be loaded via the Composer classmap though.
        Instead, the custom Autoloader, which was already in place for the WP Integration tests, has been adjusted to handle loading the correct version of the class depending on whether or not the WP version against which the tests are being run contains the (backported) changes from WP 5.9 or not.
    
    Includes minor tweak to the PHPCS config to allow for the duplicate class.
    
    Fixes 14

--- src/WPIntegration/Autoload.php
+++ src/WPIntegration/Autoload.php
@@ -7,9 +7,12 @@ use PHPUnit\Runner\Version as PHPUnit_Version;
 /**
  * Custom autoloader.
  *
- * Autoloader file for the PHPUnit 9 MockObject classes.
+ * Autoloader file for the PHPUnit 9 MockObject classes as included in WP 5.6 - 5.8
+ * and the WP Test Utils Integration test TestCase.
  *
- * Hack around PHPUnit < 9.3 mocking not being compatible with PHP 8.
+ * - Hack around PHPUnit < 9.3 mocking not being compatible with PHP >= 8.
+ * - Load the most appropriate TestCase depending on the features available in the WP version
+ *   tests are being run against.
  *
  * This allows for cross-version compatibility with various PHP, PHPUnit and WP versions.
  *
@@ -17,6 +20,7 @@ use PHPUnit\Runner\Version as PHPUnit_Version;
  * as defined in the `src/WPIntegration/bootstrap-functions.php` file to register the autoloader.
  *
  * @since 0.2.0
+ * @since 1.0.0 Now also handles the loading of the WP Test Utils Integration TestCase.
  */
 final class Autoload {
 
@@ -26,10 +30,11 @@ final class Autoload {
 	 * @var string[] => true
 	 */
 	private static $supported_classes = [
-		'PHPUnit\Framework\MockObject\Builder\NamespaceMatch'  => true,
-		'PHPUnit\Framework\MockObject\Builder\ParametersMatch' => true,
-		'PHPUnit\Framework\MockObject\InvocationMocker'        => true,
-		'PHPUnit\Framework\MockObject\MockMethod'              => true,
+		'PHPUnit\\Framework\\MockObject\\Builder\\NamespaceMatch'  => true,
+		'PHPUnit\\Framework\\MockObject\\Builder\\ParametersMatch' => true,
+		'PHPUnit\\Framework\\MockObject\\InvocationMocker'         => true,
+		'PHPUnit\\Framework\\MockObject\\MockMethod'               => true,
+		'Yoast\\WPTestUtils\\WPIntegration\\TestCase'              => true,
 	];
 
 	/**
@@ -40,9 +45,29 @@ final class Autoload {
 	 * @return bool
 	 */
 	public static function load( $class_name ) {
+		if ( isset( self::$supported_classes[ $class_name ] ) === false ) {
+			// Bow out, not a class this autoloader handles.
+			return false;
+		}
 
+		if ( \strpos( $class_name, 'PHPUnit' ) === 0 ) {
+			return self::load_mockobject_class( $class_name );
+		}
+
+		return self::load_test_case();
+	}
+
+	/**
+	 * Loads the PHPUnit 9.x mock object classes as included with WP 5.6 - 5.8
+	 * when relevant.
+	 *
+	 * @param string $class_name The name of the class to load.
+	 *
+	 * @return bool
+	 */
+	private static function load_mockobject_class( $class_name ) {
 		if ( \PHP_VERSION_ID < 80000 ) {
-			// This autoloader is only needed when the tests are being run on PHP >= 8.0.
+			// The mock object autoloading is only needed when the tests are being run on PHP >= 8.0.
 			// Let the standard Composer autoloader handle things otherwise.
 			return false;
 		}
@@ -50,13 +75,8 @@ final class Autoload {
 		if ( \class_exists( PHPUnit_Version::class ) === false
 			|| \version_compare( PHPUnit_Version::id(), '8.0.0', '>=' )
 		) {
-			// This autoloader is only needed when the tests are being run on PHPUnit < 8
+			// The mock object autoloading is only needed when the tests are being run on PHPUnit < 8
 			// and won't work with PHPUnit 5 anyway.
-			return false;
-		}
-
-		if ( isset( self::$supported_classes[ $class_name ] ) === false ) {
-			// Bow out, not a class this autoloader handles.
 			return false;
 		}
 
@@ -66,7 +86,7 @@ final class Autoload {
 			return false;
 		}
 
-		// Try getting the overloaded file as included in WP 5.6/master.
+		// Try getting the overloaded file as included in WP 5.6 - 5.8.
 		$relative_filename = \strtr( \substr( $class_name, 18 ), '\\', \DIRECTORY_SEPARATOR ) . '.php';
 		$file              = \realpath( $wp_test_dir . 'includes/phpunit7/' . $relative_filename );
 
@@ -75,6 +95,24 @@ final class Autoload {
 		}
 
 		require_once $file;
+		return true;
+	}
+
+	/**
+	 * Loads the most appropriate test case depending on the WP version the tests are
+	 * being run against.
+	 *
+	 * @return bool
+	 */
+	private static function load_test_case() {
+		if ( \method_exists( 'WP_UnitTestCase', 'set_up' ) === false ) {
+			// Older WP version from before the test changes.
+			require_once __DIR__ . '/TestCase.php';
+			return true;
+		}
+
+		// WP 5.9 or a WP 5.2 - 5.8 version which includes the Polyfills and the fixture method wrappers.
+		require_once __DIR__ . '/TestCaseNoPolyfills.php';
 		return true;
 	}
 }
