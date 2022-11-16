b4e655f706444e082a2c08c2ab2fec680ad34246
jrfnl <jrfnl@users.noreply.github.com>
2022-11-16 17:19:46 +0100

    BrainMonkey/TestCase: add new `makeDouble[s]ForUnavailableClass[es]()` functions
    These methods allow for on the fly creation of test doubles which allow for dynamic properties, which allows for bypassing the PHP 8.2 dynamic property deprecation notice when the _real_ class has these properties declared, but a mock is used in the tests.
    
    See the README docs + mockery/mockery 1197 for additional information.
    
    Includes:
    * Tests to cover the new functionality.
    * Documentation in the README file.

---
+++ tests/BrainMonkey/Fixtures/AnotherAvailableClass.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Yoast\WPTestUtils\Tests\BrainMonkey\Fixtures;
+
+/**
+ * Fixture to test the TestCase::makeDoubleForUnavailableClass() method.
+ */
+class AnotherAvailableClass {}
